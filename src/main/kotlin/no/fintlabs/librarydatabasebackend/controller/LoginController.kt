e4ec8e279f76c34dee75a56ff166f26f670a3daa
sindre <sindre.giske@novari.no>
2025-09-16 15:43:26 +0200

    [security tweak] Login changed to Post

--- src/main/kotlin/no/fintlabs/librarydatabasebackend/controller/LoginController.kt
+++ src/main/kotlin/no/fintlabs/librarydatabasebackend/controller/LoginController.kt
@@ -6,7 +6,6 @@ import no.fintlabs.librarydatabasebackend.entity.Borrower
 import no.fintlabs.librarydatabasebackend.service.BorrowerService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,8 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 class LoginController(
     private val service: BorrowerService,
 ) {
-
-    @GetMapping()
+    @PostMapping()
     fun login(
         @RequestBody request: LoginRequest
     ): ResponseEntity<Any> {
@@ -40,7 +38,7 @@ class LoginController(
         }
     }
 
-    @PostMapping()
+    @PostMapping("/register")
     fun createUser(
         @RequestBody request: CreateUserRequest
     ): ResponseEntity<Any> {
